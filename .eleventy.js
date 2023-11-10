f1bed0a0967df3a9f933a9995dcd3746a43558d8
Dillon Kelley <dkelley@wbez.org>
2023-11-10 16:26:02 -0600

    progress loading cms + content

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,41 @@
+const yaml = require("js-yaml");
+const htmlmin = require("html-minifier");
+
+
 module.exports = function (eleventyConfig) {
-
-
-
     eleventyConfig.addPassthroughCopy({
         "./src/admin/config.yml": "./admin/config.yml",
         // "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
         // "./node_modules/prismjs/themes/prism-tomorrow.css":
         //     "./static/css/prism-tomorrow.css",
         });
-    
+    eleventyConfig.addPassthroughCopy("src/static/imgs");
+    eleventyConfig.addPassthroughCopy("src/static/css");
+
+    // To Support .yaml Extension in _data
+    // You may remove this if you can use JSON
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+
+    // Minify HTML
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+        if (outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+            useShortDoctype: true,
+            removeComments: true,
+            collapseWhitespace: true,
+        });
+        return minified;
+        }
+
+        return content;
+    });
 
     // Let Eleventy transform HTML files as nunjucks
     // So that we can use .html instead of .njk
     return {
         dir: {
-        input: "src",
+            input: "src",
         },
         htmlTemplateEngine: "njk",
     };
