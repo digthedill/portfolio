e4637bf4ac63bb550f599a10c10f8e57c3d9e312
Dillon Kelley <dkelley@wbez.org>
2023-11-13 22:04:20 -0600

    tailwind + settings.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,42 +1,41 @@
 const yaml = require("js-yaml");
 const htmlmin = require("html-minifier");
 
-
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({
-        "./src/admin/config.yml": "./admin/config.yml",
-        // "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-        // "./node_modules/prismjs/themes/prism-tomorrow.css":
-        //     "./static/css/prism-tomorrow.css",
-        });
-    eleventyConfig.addPassthroughCopy("src/static/imgs");
-    eleventyConfig.addPassthroughCopy("src/static/css");
+  eleventyConfig.addPassthroughCopy({
+    "./src/admin/config.yml": "./admin/config.yml",
+    // "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
+    // "./node_modules/prismjs/themes/prism-tomorrow.css":
+    //     "./static/css/prism-tomorrow.css",
+  });
+  eleventyConfig.addPassthroughCopy("src/static/imgs");
+  eleventyConfig.addPassthroughCopy("src/static/css");
 
-    // To Support .yaml Extension in _data
-    // You may remove this if you can use JSON
-    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  // To Support .yaml Extension in _data
+  // You may remove this if you can use JSON
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
-    // Minify HTML
-    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-        // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-        if (outputPath.endsWith(".html")) {
-        let minified = htmlmin.minify(content, {
-            useShortDoctype: true,
-            removeComments: true,
-            collapseWhitespace: true,
-        });
-        return minified;
-        }
+  // Minify HTML
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
 
-        return content;
-    });
+    return content;
+  });
 
-    // Let Eleventy transform HTML files as nunjucks
-    // So that we can use .html instead of .njk
-    return {
-        dir: {
-            input: "src",
-        },
-        htmlTemplateEngine: "njk",
-    };
-}
+  // Let Eleventy transform HTML files as nunjucks
+  // So that we can use .html instead of .njk
+  return {
+    dir: {
+      input: "src",
+    },
+    htmlTemplateEngine: "njk",
+  };
+};
