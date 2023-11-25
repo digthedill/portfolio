552469d5f53af2493d020e44fb19d5305eee9ac7
Dillon Kelley <dkelley@wbez.org>
2023-11-24 21:29:28 -0600

    setup config for subdirs

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "DRK",
+  url: "https://dillon.troutmask.dev/",
+  language: "en",
+  description: "Full stack software engineer and professional hobbyist.",
+  author: {
+    name: "Dillon Kelley",
+    email: "digthedill@gmail.com",
+    url: "https://dillon.troutmask.dev/",
+  },
+};
--- src/posts/testing.md
+++ src/posts/testing.md
@@ -3,5 +3,7 @@ title: Testing
 date: 2023-11-09T15:54:47.371Z
 tags:
   - post
+layout: post
 ---
+
 Testing Data
